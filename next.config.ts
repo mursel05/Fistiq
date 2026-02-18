91ba6147674b31e9d97de22643921b110c12bb97
mursel05 <mursal.haxverdiyev@gmail.com>
2026-02-18 21:06:04 +0400

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  reactStrictMode: false,
+  devIndicators: false,
+};
+
+export default nextConfig;
